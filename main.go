2f5232564ee9690cb59e9d6b01d2f2ae254ff3d0
Rus Hughes <russell.hughes@gmail.com>
2018-10-23 16:58:49 +0900

    s6 l67

--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
+
+type logWriter struct{}
 
 func main() {
 	resp, err := http.Get("http://www.google.com")
@@ -14,5 +17,15 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Println(resp)
+	// bs := make([]byte, 99999)
+	// resp.Body.Read(bs)
+	// fmt.Println(string(bs))
+
+	lw := logWriter{}
+	io.Copy(lw, resp.Body)
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	return len(bs), nil
 }
